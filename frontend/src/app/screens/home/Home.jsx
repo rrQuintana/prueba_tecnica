9613cd0e20c1ef4e9ec545c1db0525f6b147e4c9
Roberto Quintana <rrquintana7@gmail.com>
2024-04-07 13:13:03 -0600

    fixes

--- frontend/src/app/screens/home/Home.jsx
+++ frontend/src/app/screens/home/Home.jsx
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux";
-import AnimatedPage from "../../shared/AnimatedPage"
-import NavBar from "./components/NavBar";
 import TableUsers from "./components/TableUsers";
 import useGetUsers from "../../logic/hooks/useGetUsers";
 import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
+import Layout from "../../shared/Layout";
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const user = useSelector(state => state.session.user);
-  const [ filters, setFilters ] = useState({ page: 1});
-  
+  const navigation = useNavigate();
+  const [filters, setFilters] = useState({ page: 1 });
+
   const { data: users, isLoading, refetch } = useGetUsers(filters);
-  const [ usersData, setUsersData ] = useState({ page: 1, totalPages: 1, users: [] });
+  const [usersData, setUsersData] = useState({ page: 1, totalPages: 1, users: [] });
 
   useEffect(() => {
     if (users) {
@@ -22,7 +23,7 @@ function Home() {
       });
     }
   }, [users]);
-  
+
   useEffect(() => {
     refetch();
   }, [filters, refetch]);
@@ -31,20 +32,17 @@ function Home() {
     setFilters({ ...filters, page: page });
   }
 
-  console.log(user);
-
   return (
-    <AnimatedPage>
-      <div className="w-full h-full bg-white">
-        <NavBar user={user} />
-        <div className="p-12">
-          {user.roleName === 'Admin' && ( 
-            <Button variant="contained" color="primary">Crear usuario</Button> 
-          )}
-          <TableUsers rol={user.roleName} usersData={usersData} isLoading={isLoading} handlePageChange={handlePageChange} />
-        </div>
+    <Layout>
+      <div className="px-12 py-6">
+        {user.roleName === 'Admin' && (
+          <div className="flex w-full justify-end mb-4">
+            <Button onClick={()=>navigation('/form-users')} variant="contained" color="primary">Crear usuario</Button>
+          </div>
+        )}
+        <TableUsers rol={user.roleName} usersData={usersData} isLoading={isLoading} handlePageChange={handlePageChange} refetch={refetch} />
       </div>
-    </AnimatedPage>
+    </Layout>
   )
 }
 
