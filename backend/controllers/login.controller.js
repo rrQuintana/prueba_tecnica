c41dda95bb915606938b7f6bb1ff6f7cde6479bd
Roberto Quintana <rrquintana7@gmail.com>
2024-04-08 15:36:56 -0600

    login error handler

--- backend/controllers/login.controller.js
+++ backend/controllers/login.controller.js
@@ -2,10 +2,20 @@ const LoginService = require('../services/login.service');
 
 const loginService = new LoginService();
 
-const login = async (req, res) => {
-  const response = await loginService.login(req.body);
-  res.json(response);
-};
+async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    const loginResponse = await loginService.login({ email, password });
+
+    res.status(200).json(loginResponse);
+  } catch (error) {
+    if (error.message === 'Credenciales inválidas') {
+      res.status(401).json({ error: 'Credenciales inválidas' });
+    }
+    next(error);
+  }
+}
 
 module.exports = {
   login,
